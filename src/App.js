75b641b5821751869a145db7a7dd462ccf79c15e
Bfrpaulon <122818137+Bfrpaulon@users.noreply.github.com>
2023-07-19 00:27:50 +0100

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import "./style.css";
-import 'framer-motion';
-export default function App() {
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import ArticlesPage from './components/ArticlesPage';
+import ArticlePage from './components/ArticlePage';
+import CategoriesPage from './components/CategoriesPage';
+import CategoryPage from './components/CategoryPage';
+import ContactPage from './components/ContactPage';
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see  magic happen :)</p>
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/articles" component={ArticlesPage} />
+      <Route path="/article/:id" component={ArticlePage} />
+      <Route path="/categories" component={CategoriesPage} />
+      <Route path="/category/:id" component={CategoryPage} />
+      <Route path="/contact" component={ContactPage} />
+    </Switch>
   );
-}
+};
+
+export default App;
